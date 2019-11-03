8c04c1c8f751dd86bb151e2fb0e06675bda90692
qvdang96 <qvdang96@gmail.com>
2019-11-03 12:19:51 +0700

    Add and test cmake file

---
+++ src/formatter.cpp
@@ -0,0 +1,6 @@
+#include "formatter.h"
+
+int sum( int a, int b)
+{
+    return a + b +10;
+}
---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include "formatter.h"
+
+using namespace std;
+
+int main()
+{
+
+    int a,b;
+    int c;
+    a = 10; 
+    b = 13;
+
+    cout << a << endl;
+
+    c =  sum(a,b);
+    return 0;
+}
